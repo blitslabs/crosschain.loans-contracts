a5108a9ab2b8597acca6f20332c5ca3767a274f5
blitslabs <defi@blits.net>
2021-01-20 20:47:14 -0600

    ETH/add_asset_type script

---
+++ scripts/eth/add_asset_type.js
@@ -0,0 +1,71 @@
+require('dotenv').config()
+const Web3 = require('web3')
+const Tx = require('ethereumjs-tx').Transaction
+const {
+    ETH_HTTP_PROVIDER, ETH_CHAIN_ID,
+    ETH_CHAIN_NAME, ETH_PUBLIC_KEY,
+    ETH_PRIVATE_KEY, ETH_LOANS_CONTRACT,
+} = process.env
+const LOANS_CONTRACT_ABI = (require('../../build/contracts/CrosschainLoans.json')).abi
+
+const addAssetType = async (
+    contractAddress,
+    maxLoanAmount, minLoanAmount,
+    baseRatePerYear,
+    multiplierPerYear
+) => {
+    const web3 = new Web3(new Web3.providers.HttpProvider(ETH_HTTP_PROVIDER))
+    const contract = new web3.eth.Contract(LOANS_CONTRACT_ABI, ETH_LOANS_CONTRACT, { from: ETH_PUBLIC_KEY })
+    const nonce = await web3.eth.getTransactionCount(ETH_PUBLIC_KEY)
+
+    const data = await contract.methods.addAssetType(
+        contractAddress,
+        maxLoanAmount,
+        minLoanAmount,
+        baseRatePerYear,
+        multiplierPerYear,
+    ).encodeABI()
+
+    const rawTx = {
+        from: ETH_PUBLIC_KEY,
+        nonce: '0x' + nonce.toString(16),
+        gasPrice: '0x003B9ACA00',
+        gasLimit: '0x170D62',
+        to: ETH_LOANS_CONTRACT,
+        value: '0x0',
+        chainId: ETH_CHAIN_ID,
+        data: data
+    }
+
+    const tx = new Tx(rawTx, { chain: ETH_CHAIN_NAME })
+    const privateKey = new Buffer.from(ETH_PRIVATE_KEY, 'hex')
+    tx.sign(privateKey)
+    const serializedTx = tx.serialize()
+
+    try {
+        const response = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+        return { status: 'OK', payload: response, message: 'Transaction sent' }
+    } catch (e) {
+        return { status: 'ERROR', message: e }
+    }
+}
+
+const start = async () => {
+    const contractAddress = '0x8ffDAb0C1e1264983BedB3692D6eE930B2488A68'
+    const maxLoanAmount = '1000000000000000000000' // 10000
+    const minLoanAmount = '100000000000000000000' // 20
+    const baseRatePerYear = '55000000000000000' // 0.055
+    const multiplierPerYear = '1100000000000000000' // 1.1
+
+    const response = await addAssetType(
+        contractAddress,
+        maxLoanAmount,
+        minLoanAmount,
+        baseRatePerYear,
+        multiplierPerYear
+    )
+
+    console.log(response)
+}
+
+start()
