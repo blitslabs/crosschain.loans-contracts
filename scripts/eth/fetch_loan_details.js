c4c77d94112cbc175779a4779b3322bfb8782830
blitslabs <defi@blits.net>
2021-01-20 22:20:57 -0600

    ethereum fetch loan details script

---
+++ scripts/eth/fetch_loan_details.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const Web3 = require('web3')
+const Tx = require('ethereumjs-tx').Transaction
+const {
+    ETH_HTTP_PROVIDER, ETH_CHAIN_ID,
+    ETH_CHAIN_NAME, ETH_PUBLIC_KEY,
+    ETH_PRIVATE_KEY, ETH_LOANS_CONTRACT,
+} = process.env
+const LOANS_CONTRACT_ABI = (require('../../build/contracts/CrosschainLoans.json')).abi
+
+const fetchLoan = async (loanId) => {
+    const web3 = new Web3(new Web3.providers.HttpProvider(ETH_HTTP_PROVIDER))
+    const contract = new web3.eth.Contract(LOANS_CONTRACT_ABI, ETH_LOANS_CONTRACT, { from: ETH_PUBLIC_KEY })
+    return await contract.methods.fetchLoan(loanId).call()
+}
+
+const start = async () => {
+    const loanId = '1'
+    const response = await fetchLoan(loanId)
+    console.log(response)
+}
+
+start()
